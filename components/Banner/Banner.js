d65f4695dbaee207e4535a8628c1602d7ca4e45d
Omar <omaar_5@hotmail.com>
2021-08-14 14:45:05 +0200

    part-1: Banner

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,11 +1,42 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {
     Container,
+    ImgContainer,
+    Img,
 } from "./styledBanner";
 
 export const Banner = () => {
     return (
         <Container>
-            Banner
+            <Carousel
+                infiniteLoop
+                autoplay={true}
+                interval={1000}
+                dynamicHeight={true}
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+            >
+                <ImgContainer>
+                    <Img src="/banner/2.jpg" />
+                </ImgContainer>
+                <ImgContainer>
+                    <Img src="/banner/1.jpg" />
+                </ImgContainer>
+                <ImgContainer>
+                    <Img src="/banner/3.jpg" />
+                </ImgContainer>
+                <ImgContainer>
+                    <Img src="/banner/4.jpg" />
+                </ImgContainer>
+                <ImgContainer>
+                    <Img src="/banner/5.jpg" />
+                </ImgContainer>
+                <ImgContainer>
+                    <Img src="/banner/6.jpg" />
+                </ImgContainer>
+            </Carousel>
         </Container>
     );
 };
--- components/Banner/styledBanner.js
+++ components/Banner/styledBanner.js
@@ -1,7 +1,64 @@
 import styled from 'styled-components';
-import { headerHeight } from '../../Global/GlobalStyle';
+import { headerHeight, lgScreen, mdScreen } from '../../Global/GlobalStyle';
 
 export const Container = styled.div`
   margin-top: calc(${headerHeight} * 2);
   padding-top: 1rem;
+  position: relative;
+
+  .carousel {
+    display: none;
+
+    @media (min-width: ${lgScreen}px) {
+        display: block;
+    }
+  }
+
+  .carousel li img {
+    height: 400px;
+    object-fit: contain;
+  }
+
+.carousel .control-next.control-arrow, .carousel .control-next.control-arrow:hover{
+  background-color: transparent;
+  right: 20px;
+}
+
+.carousel .control-prev.control-arrow, .carousel .control-prev.control-arrow:hover {
+  background-color: transparent;
+  left: 20px;
+}
+
+.carousel .control-arrow, .carousel.carousel-slider .control-arrow{
+  opacity: 0.6;
+}
+
+.carousel .control-prev.control-arrow:hover,
+.carousel .control-next.control-arrow:hover {
+    opacity: 1;
+}
+
+.carousel .control-next.control-arrow:before {
+  content: '';
+  border: solid ${({ theme }) => theme.colors.textColor};
+  border-width: 0 8px 8px 0;
+  display: inline-block;
+  padding: 10px;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+.carousel .control-prev.control-arrow:before {
+  content: '';
+  border: solid ${({ theme }) => theme.colors.textColor};
+  border-width: 0 8px 8px 0;
+  display: inline-block;
+  padding: 10px;
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+}
 `;
+
+export const ImgContainer = styled.div``;
+
+export const Img = styled.img``;
