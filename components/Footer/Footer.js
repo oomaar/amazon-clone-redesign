a6d92f0c21af91e86adf6f25a09ac572d127ef8a
Omar <omaar_5@hotmail.com>
2021-08-15 16:22:32 +0200

    refactor: Footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,27 +1,74 @@
+import { GoChevronRight } from "react-icons/go";
 import Image from "next/image";
 import {
     FooterContainer,
+    FooterSubContainer,
+    FooterLinks,
+    FooterLink,
+    CopyContainer,
+    FooterLogo,
+    FooterCopy,
 } from "./styledFooter";
 
 export const Footer = ({ darkMode }) => {
     return (
         <FooterContainer>
-            {darkMode === 'light' ? (
-                <Image
-                    src="/logo-single-black.png"
-                    width={50}
-                    height={50}
-                    objectFit="contain"
-                />
-            ) : (
-                <Image
-                    src="/logo-single-white.png"
-                    width={50}
-                    height={50}
-                    objectFit="contain"
-                />
-            )}
-            <h6>Amazon Clone, Powered by NextJS</h6>
+            <FooterSubContainer>
+                <FooterLinks>
+                    <h3>Get to Know Us</h3>
+                    <FooterLink>About Amazon</FooterLink>
+                    <FooterLink>Connect with Us</FooterLink>
+                    <FooterLink>Amazon Careers</FooterLink>
+                    <FooterLink>Gift a Smile</FooterLink>
+                </FooterLinks>
+                <FooterLinks>
+                    <h3>Make Money with Us</h3>
+                    <FooterLink>Products on Amazon</FooterLink>
+                    <FooterLink>Apps on Amazon</FooterLink>
+                    <FooterLink>Amazon e-business</FooterLink>
+                    <FooterLink>Advertise Your Products</FooterLink>
+                    <FooterLink>Self-Publish with Us</FooterLink>
+                    <FooterLink>Host on Amazon Hub</FooterLink>
+                    <FooterLink><GoChevronRight /> See More</FooterLink>
+                </FooterLinks>
+                <FooterLinks>
+                    <h3>Amazon Payment</h3>
+                    <FooterLink>Amazon Business Card</FooterLink>
+                    <FooterLink>Shop with Points</FooterLink>
+                    <FooterLink>Reload Your Balance</FooterLink>
+                    <FooterLink>Amazon Currency Converter</FooterLink>
+                </FooterLinks>
+                <FooterLinks>
+                    <h3>Let Us Help You</h3>
+                    <FooterLink>Amazon with COVID-19</FooterLink>
+                    <FooterLink>Shipping Rules & Policies</FooterLink>
+                    <FooterLink>Returns & Replacements</FooterLink>
+                    <FooterLink>Manage Your Devices</FooterLink>
+                    <FooterLink>Amazon Assistant</FooterLink>
+                </FooterLinks>
+            </FooterSubContainer>
+            <CopyContainer>
+                <FooterLogo>
+                    {darkMode === 'light' ? (
+                        <Image
+                            src="/logo-single-black.png"
+                            width={50}
+                            height={50}
+                            objectFit="contain"
+                        />
+                    ) : (
+                        <Image
+                            src="/logo-single-white.png"
+                            width={50}
+                            height={50}
+                            objectFit="contain"
+                        />
+                    )}
+                </FooterLogo>
+                <FooterCopy>
+                    Amazon Clone | Powered by NextJS | Developed & Re-Designed by <span>Omar Hassan</span>
+                </FooterCopy>
+            </CopyContainer>
         </FooterContainer>
     );
 };
--- components/Footer/styledFooter.js
+++ components/Footer/styledFooter.js
@@ -1,19 +1,62 @@
 import styled from 'styled-components';
-import { mdScreen, sidebarWidth } from '../../Global/GlobalStyle';
+import { lgScreen, mdScreen, sidebarWidth, xlScreen } from '../../Global/GlobalStyle';
 
 export const FooterContainer = styled.footer`
-  padding: 1rem 0 2rem;
-  background-color: ${({ theme }) => theme.colors.bodyColor};
+  display: flex;
+  flex-direction: column;
+  padding: 0 1rem 5rem;
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  color: ${({ theme }) => theme.colors.textColor};
+
+  @media screen and (min-width: ${mdScreen}px) {
+    margin-left: ${sidebarWidth};
+  }
+`;
+
+export const FooterSubContainer = styled.div`
+  display: grid;
+  gap: 1.5rem;
+  grid-template-columns: repeat(2, 2fr);
+  place-items: center;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    grid-template-columns: repeat(3, 2fr);
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    grid-template-columns: repeat(4, 2fr);
+  }
+`;
+
+export const FooterLinks = styled.div`
+  height: 25rem;
+
+  h3 {
+    font-size: ${({ theme }) => theme.typography.normalFontSize};
+  }
+`;
+
+export const FooterLink = styled.p`
+  font-size: ${({ theme }) => theme.typography.smallFontSize};
+`;
+
+export const CopyContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.4);
+  border-top: 2px solid rgba(229, 231, 235, 0.9);
 
-  @media screen and (min-width: ${mdScreen}px) {
-    padding: 1rem;
+  @media screen and (min-width: ${lgScreen}px) {
     flex-direction: row;
-    justify-content: space-between;
-    margin-left: ${sidebarWidth};
-    height: 4rem;
+    justify-content: space-around;
+    width: 40%;
+  }
+`;
+
+export const FooterLogo = styled.div``;
+
+export const FooterCopy = styled.h6`
+  span {
+    color: ${({ theme }) => theme.colors.firstColor};
   }
 `;
