bf9872ab742595db47ce5a4fe891ddb50fcd4b91
Omar <omaar_5@hotmail.com>
2021-08-15 14:01:00 +0200

    part-3: ProductFeed

--- components/ProductFeed/ProductFeed.js
+++ components/ProductFeed/ProductFeed.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { Product } from "..";
+import { FaBars } from "react-icons/fa";
 import {
     Container,
+    Nav,
+    NavIcon,
+    List,
+    Item,
 } from "./styledProductFeed";
 
 export const ProductFeed = ({ products }) => {
+    const [activeCategory, setActiveCategory] = useState("all");
+    const [showList, setShowList] = useState(false);
+
     const productsComponent = products.map(({
         id,
         title,
@@ -23,9 +32,75 @@ export const ProductFeed = ({ products }) => {
         />
     ));
 
+    const filtering = products.map(({ id,
+        title,
+        price,
+        description,
+        category,
+        image }) => (
+        category === activeCategory && <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+        />
+    ));
+
     return (
-        <Container>
-            {productsComponent}
-        </Container>
+        <div>
+            <Nav>
+                <NavIcon onClick={() => setShowList(state => !state)}>
+                    <FaBars />
+                </NavIcon>
+                <List showList={showList}>
+                    <li>
+                        <Item
+                            onClick={() => setActiveCategory("all")}
+                            className={`${activeCategory === "all" && 'active__link'}`}
+                        >
+                            All
+                        </Item>
+                    </li>
+                    <li>
+                        <Item
+                            onClick={() => setActiveCategory("electronics")}
+                            className={`${activeCategory === "electronics" && 'active__link'}`}
+                        >
+                            Electronics
+                        </Item>
+                    </li>
+                    <li>
+                        <Item
+                            onClick={() => setActiveCategory("men's clothing")}
+                            className={`${activeCategory === "men's clothing" && 'active__link'}`}
+                        >
+                            Men's Clothing
+                        </Item>
+                    </li>
+                    <li>
+                        <Item
+                            onClick={() => setActiveCategory("women's clothing")}
+                            className={`${activeCategory === "women's clothing" && 'active__link'}`}
+                        >
+                            Women's Clothing
+                        </Item>
+                    </li>
+                    <li>
+                        <Item
+                            onClick={() => setActiveCategory("jewelery")}
+                            className={`${activeCategory === "jewelery" && 'active__link'}`}
+                        >
+                            Jewelery
+                        </Item>
+                    </li>
+                </List>
+            </Nav>
+            <Container>
+                {activeCategory === "all" ? productsComponent : filtering}
+            </Container>
+        </div>
     );
 };
--- components/ProductFeed/styledProductFeed.js
+++ components/ProductFeed/styledProductFeed.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
-import { lgScreen, mdScreen, xlScreen } from '../../Global/GlobalStyle';
+import { headerHeight, lgScreen, mdScreen, sidebarWidth, xlScreen } from '../../Global/GlobalStyle';
 
 export const Container = styled.div`
   display: grid;
   gap: 2rem;
   grid-template-columns: repeat(1, minmax(0, 1fr));
-  padding: 1rem 1rem 5rem;
+  padding: 5rem 1rem 5rem;
+  position: relative;
   
   @media screen and (min-width: ${mdScreen}px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -17,5 +18,82 @@ export const Container = styled.div`
 
   @media screen and (min-width: ${xlScreen}px) {
     grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+`;
+
+export const Nav = styled.nav`
+  padding: 1rem 0.5rem 0.5rem;
+  position: fixed;
+  background-color: ${({ theme }) => theme.colors.bodyColor};
+  z-index: 100;
+  top: 6rem;
+  right: 0;
+  left: 0;
+  
+  @media screen and (min-width: ${mdScreen}px) {
+    position: sticky;
+    top: 6.03rem;
+    left: calc(${sidebarWidth} + 1rem);
+  }
+`;
+
+export const List = styled.ul`
+  display: ${({ showList }) => showList ? "flex" : "none"};
+  flex-direction: column;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.titleColor};
+  align-items: center;
+  margin: 0;
+    
+  .active__link {
+    border-bottom: 3px solid ${({ theme }) => theme.colors.titleColor};
+    opacity: 1;
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    border: 0;
+  }
+`;
+
+export const NavIcon = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+  width: 10%;
+  margin-left: auto;
+  font-size: 1.1rem;
+  color: ${({ theme }) => theme.colors.firstColor};
+
+  @media screen and (min-width: ${mdScreen}px) {
+    display: none;
+  }
+`;
+
+export const Item = styled.p`
+  font-size: ${({ theme }) => theme.typography.normalFontSize};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontSemiBold};
+  color: ${({ theme }) => theme.colors.titleColor};
+  transition: 0.5s;
+  cursor: pointer;
+  padding: 0 0.5rem 0.5rem;
+  border-bottom: 3px solid transparent;
+  margin: 0.5rem;
+  opacity: 0.4;
+
+  :hover {
+    border-bottom: 3px solid ${({ theme }) => theme.colors.titleColor};
+    opacity: 1;
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    margin: 0;
+    font-size: ${({ theme }) => theme.typography.smallFontSize};
+  }
+
+  @media screen and (min-width: ${lgScreen}px) {
+    font-size: ${({ theme }) => theme.typography.h3FontSize};
   }
 `;
