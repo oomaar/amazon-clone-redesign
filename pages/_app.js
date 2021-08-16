5dd073a64fe868e49f9b53bf600a38b327daf1c4
Omar <omaar_5@hotmail.com>
2021-08-16 15:23:48 +0200

    refactor: Master Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { Footer, SearchBar, Sidebar } from "../components";
+import { darkTheme, GlobalStyle, lightTheme, theme } from "../Global/GlobalStyle";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [darkMode, setDarkMode] = useState('light');
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ThemeProvider theme={darkMode === 'light' ? lightTheme : darkTheme}>
+        <div>
+          <GlobalStyle />
+          <Sidebar darkMode={darkMode} />
+          <SearchBar darkMode={darkMode} />
+          <Component {...pageProps} />
+          <Footer darkMode={darkMode} />
+        </div>
+      </ThemeProvider>
+    </ThemeProvider>
+  );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,18 @@
-import { useState } from "react";
-import styled, { ThemeProvider } from 'styled-components';
-import { Banner, Footer, HeadTag, ProductFeed, SearchBar, Sidebar } from "../components";
-import { darkTheme, GlobalStyle, lightTheme, mdScreen, theme, xlScreen } from "../Global/GlobalStyle";
+import styled from 'styled-components';
+import { Banner, HeadTag, ProductFeed } from "../components";
+import { mdScreen, xlScreen } from "../Global/GlobalStyle";
 
 export default function Home({ products }) {
-  const [darkMode, setDarkMode] = useState('light');
 
   return (
-    <ThemeProvider theme={theme}>
-      <ThemeProvider theme={darkMode === 'light' ? lightTheme : darkTheme}>
-        <GlobalStyle />
-        <Application>
-          <HeadTag />
+    <Application>
+      <HeadTag />
 
-          <Sidebar darkMode={darkMode} />
-          <SearchBar darkMode={darkMode} />
-          <Main>
-            <Banner />
-            <ProductFeed products={products} />
-          </Main>
-          <Footer darkMode={darkMode} />
-        </Application>
-      </ThemeProvider>
-    </ThemeProvider>
+      <Main>
+        <Banner />
+        <ProductFeed products={products} />
+      </Main>
+    </Application>
   );
 };
 
