0ebce884f39ad47fd20819aa9519b45c34c8b4d0
Omar <omaar_5@hotmail.com>
2021-08-16 23:10:19 +0200

    refactor: Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import Layout from "../Global/Layout";
+
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-
   );
 };
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,29 +1,15 @@
+import { HeadTag } from "../../Global/HeadTag";
+
 const Product = ({ product }) => {
     return (
         <div>
+            <HeadTag title={`Product | ${product.category}`} />
             Product Page
         </div>
     );
 };
 
 export default Product;
-
-// export async function getStaticPaths() {
-//     const res = await fetch("https://fakestoreapi.com/products");
-//     const data = await res.json();
-
-//     const paths = data.map(product => {
-//         return {
-//             params: { id: product.id.toString() }
-//         };
-//     });
-
-//     return {
-//         paths,
-//         // This Mistake made me stuck for 1 day !
-//         fallBack: false
-//     };
-// };
 
 export const getStaticPaths = async () => {
     const res = await fetch(`https://fakestoreapi.com/products`);
@@ -50,3 +36,20 @@ export const getStaticProps = async (context) => {
         props: { product: data }
     };
 };
+
+// export async function getStaticPaths() {
+//     const res = await fetch("https://fakestoreapi.com/products");
+//     const data = await res.json();
+
+//     const paths = data.map(product => {
+//         return {
+//             params: { id: product.id.toString() }
+//         };
+//     });
+
+//     return {
+//         paths,
+//         // This Mistake made me stuck for 1 day !
+//         fallBack: false
+//     };
+// };
