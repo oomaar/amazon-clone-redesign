890ea9dd4133d380025daa080bca3684eeee2cc9
Omar <omaar_5@hotmail.com>
2021-08-16 14:34:22 +0200

    fix: console error

--- components/HeadTag/HeadTag.js
+++ components/HeadTag/HeadTag.js
@@ -8,7 +8,7 @@ export const HeadTag = () => {
 
             {/* Google Roboto Font */}
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
         </Head>
     );
