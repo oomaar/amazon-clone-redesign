5e0e68367cef58bfe5e6ceb884b37e8eb2dd52e2
Omar <omaar_5@hotmail.com>
2021-08-17 21:41:59 +0200

    use: redux-persist

--- Redux/store.js
+++ Redux/store.js
@@ -1,10 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 import darkSliceReducer from "./slices/darkSlice";
 import cartReducer from "./slices/cartSlice";
 
-export const store = configureStore({
-    reducer: {
-        dark: darkSliceReducer,
-        cart: cartReducer,
-    },
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+    dark: darkSliceReducer,
+    cart: cartReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export let persistor = persistStore(store);
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import { Provider } from "react-redux";
-import { store } from "../Redux/store";
+import { persistor, store } from "../Redux/store";
 import Layout from "../Global/Layout";
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
     </Provider>
   );
 };
