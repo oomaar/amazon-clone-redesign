79564c8dd4b2051ca58a857036b60a9680dd95b1
Omar <omaar_5@hotmail.com>
2021-08-17 03:21:53 +0200

    complete: dynamin SingleProduct Page part-1

---
+++ Global/styles/styledProductPage.js
@@ -0,0 +1,180 @@
+import styled from 'styled-components';
+import { headerHeight, lgScreen, mdScreen, xlScreen } from "../GlobalStyle";
+
+// Layout
+
+export const Container = styled.main`
+  max-width: 1024px;
+  margin-top: calc(${headerHeight} * 2);
+  display: flex;
+  flex-direction: column;
+  padding-top: 2rem;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    margin-left: 4.5rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    margin: calc(${headerHeight} * 2) auto 0 4.5rem;
+  }
+
+  @media screen and (min-width: 1160px) {
+    margin: calc(${headerHeight} * 2) auto;
+  }
+`;
+
+export const ProductContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 3rem;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 1rem;
+  }
+`;
+
+export const SubContainer = styled.div``;
+
+// El Container el fo2
+
+export const ImageContainer = styled.div`
+  width: 90%;
+  box-shadow: ${({ theme }) => theme.colors.shadowColor};
+  border-radius: 1rem;
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.inputColor};
+
+  @media screen and (min-width: ${lgScreen}px) {
+    width: 40%;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  width: 90%;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    width: 50%;
+  }
+`;
+
+export const Title = styled.h3`
+  font-size: ${({ theme }) => theme.typography.h3FontSize};
+  color: ${({ theme }) => theme.colors.titleColor};
+`;
+
+export const Description = styled.p`
+  font-size: ${({ theme }) => theme.typography.normalFontSize};
+  color: ${({ theme }) => theme.colors.textColor};
+`;
+
+export const PriceContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Discount = styled.p`
+  font-size: 1.1rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontBold};
+  color: ${({ theme }) => theme.colors.textColor};
+`;
+
+export const Price = styled.p`
+  font-size: 0.8rem;
+  color: #f00;
+  text-decoration: line-through;
+  margin-left: 0.5rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeight.SemiBold};
+`;
+
+export const PrimeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 70px;
+  }
+`;
+
+export const PrimeSubContainer = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.textColorLight};
+
+  p {
+    margin-left: 0.5rem;
+  }
+`;
+
+export const PrimeIcon = styled.div`
+  font-size: 0.7rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const AddButton = styled.button`
+  border: 0;
+  outline: 0;
+  font-size: 0.9rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontBold};
+  background-color: hsl(32, 80%, 74%);
+  padding: 0.45rem 0.9rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  color: #fff;
+  opacity: 0.8;
+  transition: 0.5s;
+  margin-right: 1rem;
+
+  :hover {
+    opacity: 1;
+  }
+`;
+
+export const BookMark = styled.div`
+  background: ${({ theme }) => theme.colors.containerColorSecond};
+  padding: 0.3rem;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const BookIcon = styled.div`
+  color: #fff;
+`;
+
+// El Container el ta7t
+
+export const AlsoLikeContainer = styled.div``;
+
+export const HeadingText = styled.h3``;
+
+// bdal kol da 7awel tgeb kam product component gahzen w zai el fol malhom !
+export const SubImageContainer = styled.div``;
+
+export const SubImageText = styled.h3``;
+
+export const LikeProduct = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  padding: 5rem 1rem 5rem;
+  position: relative;
+  
+  @media screen and (min-width: ${mdScreen}px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media screen and (min-width: ${lgScreen}px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+`;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,15 +1,129 @@
+import Image from "next/image";
+import { useState } from "react";
+import Currency from "react-currency-formatter";
 import { HeadTag } from "../../Global/HeadTag";
+import { FaLocationArrow } from "react-icons/fa";
+import { MdAddShoppingCart } from "react-icons/md";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { Product } from "../../components";
+import {
+    Container,
+    ProductContainer,
+    SubContainer,
+    ImageContainer,
+    InfoContainer,
+    Title,
+    Description,
+    PriceContainer,
+    Price,
+    Discount,
+    PrimeContainer,
+    PrimeSubContainer,
+    PrimeIcon,
+    ButtonContainer,
+    AddButton,
+    BookMark,
+    BookIcon,
+    AlsoLikeContainer,
+    HeadingText,
+    LikeProduct,
+} from '../../Global/styles/styledProductPage';
 
-const Product = ({ product }) => {
+const SingleProduct = ({ product, products }) => {
+    console.log("🚀 ~ file: [id].js ~ line 33 ~ SingleProduct ~ products", products);
+    const [hasPrime] = useState(Math.random() < 0.5);
+    const price = product.price;
+    const discount = price > 100 ? (
+        Math.floor(price - (30 / price * 100))
+    ) : (
+        Math.abs(Math.floor(price - (1 / price * 100)))
+    );
+    const randomProductMin = Math.floor(Math.random() * 10) + 1;
+    const randomProductMax = Math.floor(Math.random() * 10) + 10;
+    const productComponent = products.map(product => (
+        <Product
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            category={product.category}
+            image={product.image}
+        />
+    ));
+
     return (
         <div>
             <HeadTag title={`Product | ${product.category}`} />
-            Product Page
+
+            <Container>
+                {/* TopBox */}
+                <ProductContainer>
+                    {/* Left Box */}
+                    <ImageContainer>
+                        <Image
+                            src={product.image}
+                            width={300}
+                            height={500}
+                            objectFit="contain"
+                        />
+                    </ImageContainer>
+
+                    {/* RightBox */}
+                    <InfoContainer>
+                        <Title>{product.title}</Title>
+                        <Description>{product.description}.</Description>
+                        <PriceContainer>
+                            <Discount>
+                                <Currency quantity={discount} currency='EGP' />
+                            </Discount>
+                            <Price>
+                                <Currency quantity={price} currency='EGP' />
+                            </Price>
+                        </PriceContainer>
+                        {hasPrime && (
+                            <PrimeContainer>
+                                <img src="/prime-tag.png" alt="" />
+                                <PrimeSubContainer>
+                                    <PrimeIcon>
+                                        <FaLocationArrow />
+                                    </PrimeIcon>
+                                    <p>
+                                        Free Delivery Available - Cairo, Egypt 11361
+                                    </p>
+                                </PrimeSubContainer>
+                            </PrimeContainer>
+                        )}
+                        <ButtonContainer>
+                            <AddButton>
+                                <MdAddShoppingCart />
+                                <span>Add to Cart</span>
+                            </AddButton>
+                            <BookMark>
+                                <BookIcon>
+                                    <BsBookmark />
+                                    {/* <BsBookmarkFill /> */}
+                                </BookIcon>
+                            </BookMark>
+                        </ButtonContainer>
+                    </InfoContainer>
+                </ProductContainer>
+
+                {/* Bottom Box */}
+                <SubContainer>
+                    <AlsoLikeContainer>
+                        <HeadingText>You Might also Like</HeadingText>
+                        <LikeProduct>
+                            {productComponent.slice(randomProductMin, randomProductMax)}
+                        </LikeProduct>
+                    </AlsoLikeContainer>
+                </SubContainer>
+            </Container>
         </div>
     );
 };
 
-export default Product;
+export default SingleProduct;
 
 export const getStaticPaths = async () => {
     const res = await fetch(`https://fakestoreapi.com/products`);
@@ -31,9 +145,11 @@ export const getStaticProps = async (context) => {
     const id = context.params.id;
     const res = await fetch(`https://fakestoreapi.com/products/${id}`);
     const data = await res.json();
+    const likes = await fetch(`https://fakestoreapi.com/products`);
+    const products = await likes.json();
 
     return {
-        props: { product: data }
+        props: { product: data, products }
     };
 };
 
