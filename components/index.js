f8f8670e12fd921cf916c159a1548a7dda8dbd0c
Omar <omaar_5@hotmail.com>
2021-08-26 22:43:08 +0200

    refactor: Components

--- components/index.js
+++ components/index.js
@@ -3,5 +3,5 @@ export { SearchBar } from "./SearchBar/SearchBar";
 export { Banner } from "./Banner/Banner";
 export { ProductFeed } from "./ProductFeed/ProductFeed";
 export { Product } from "./Product/Product";
-export { Footer } from "./Footer/Footer";
 export { CartProduct } from "./CartProduct/CartProduct";
+export { Footer } from "./Footer/Footer";
