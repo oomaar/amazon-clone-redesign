ba400209de0c4c9768a1a1bfe71ad148e5c71177
Omar <omaar_5@hotmail.com>
2021-09-02 21:48:54 +0200

    update: cartSlice logic

--- Redux/slices/cartSlice.js
+++ Redux/slices/cartSlice.js
@@ -1,37 +1,68 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    items: [],
+    cartItems: [],
+    cartTotalQuantity: 0,
+    cartTotalAmount: 0
 };
 
-export const cartSlice = createSlice({
+const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            state.items = [...state.items, action.payload];
-        },
-        removeFromCart: (state, action) => {
-            const index = state.items.findIndex(cartItem => cartItem.id === action.payload.id);
-            let newCart = [...state.items];
+            const itemIndex = state.cartItems.findIndex(
+                item => item.id === action.payload.id
+            );
 
-            if (index <= 0) {
-                // The item exists in the cart, remove it
-                newCart.splice(index, 1);
+            if (itemIndex >= 0) {
+                state.cartItems[itemIndex].cartQuantity += 1;
             } else {
-                console.warn(
-                    `Cant remove product (id: ${action.payload.id}) as it's not in the cart`
-                );
+                const tempProduct = { ...action.payload, cartQuantity: 1 };
+                state.cartItems.push(tempProduct);
             };
-
-            state.items = newCart;
-        },
-    },
+        }
+    }
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
-
-export const selectItems = (state) => state.cart.items;
-export const selectTotal = (state) => state.cart.items.reduce((total, item) => total + item.price, 0);
-
+export const selectItems = (state) => state.cart.cartItems;
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
+
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//     items: [],
+// };
+
+// export const cartSlice = createSlice({
+//     name: "cart",
+//     initialState,
+//     reducers: {
+//         addToCart: (state, action) => {
+//             state.items = [...state.items, action.payload];
+//         },
+//         removeFromCart: (state, action) => {
+//             const index = state.items.findIndex(cartItem => cartItem.id === action.payload.id);
+//             let newCart = [...state.items];
+
+//             if (index <= 0) {
+//                 // The item exists in the cart, remove it
+//                 newCart.splice(index, 1);
+//             } else {
+//                 console.warn(
+//                     `Cant remove product (id: ${action.payload.id}) as it's not in the cart`
+//                 );
+//             };
+
+//             state.items = newCart;
+//         },
+//     },
+// });
+
+// export const { addToCart, removeFromCart } = cartSlice.actions;
+
+// export const selectItems = (state) => state.cart.items;
+// export const selectTotal = (state) => state.cart.items.reduce((total, item) => total + item.price, 0);
+
+// export default cartSlice.reducer;
