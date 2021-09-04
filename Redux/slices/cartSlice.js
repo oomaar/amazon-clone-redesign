db9c91fa5ae72830b0c1c2ed881b59e0d14e6e97
Omar <omaar_5@hotmail.com>
2021-09-04 10:48:24 +0200

    update: removeFromCart action logic

--- Redux/slices/cartSlice.js
+++ Redux/slices/cartSlice.js
@@ -21,48 +21,26 @@ const cartSlice = createSlice({
                 const tempProduct = { ...action.payload, cartQuantity: 1 };
                 state.cartItems.push(tempProduct);
             };
-        }
+        },
+        removeFromCart: (state, action) => {
+            const index = state.cartItems.findIndex(cartItem => cartItem.id === action.payload.id);
+            let newCart = [...state.cartItems];
+
+            if (index <= 0) {
+                // The item exists in the cart, remove it
+                newCart.splice(index, 1);
+                state.cartItems = newCart;
+            } else {
+                console.warn(
+                    `Cant remove product (id: ${action.payload.id}) as it's not in the cart`
+                );
+            };
+
+            return state;
+        },
     }
 });
 
 export const selectItems = (state) => state.cart.cartItems;
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//     items: [],
-// };
-
-// export const cartSlice = createSlice({
-//     name: "cart",
-//     initialState,
-//     reducers: {
-//         addToCart: (state, action) => {
-//             state.items = [...state.items, action.payload];
-//         },
-//         removeFromCart: (state, action) => {
-//             const index = state.items.findIndex(cartItem => cartItem.id === action.payload.id);
-//             let newCart = [...state.items];
-
-//             if (index <= 0) {
-//                 // The item exists in the cart, remove it
-//                 newCart.splice(index, 1);
-//             } else {
-//                 console.warn(
-//                     `Cant remove product (id: ${action.payload.id}) as it's not in the cart`
-//                 );
-//             };
-
-//             state.items = newCart;
-//         },
-//     },
-// });
-
-// export const { addToCart, removeFromCart } = cartSlice.actions;
-
-// export const selectItems = (state) => state.cart.items;
-// export const selectTotal = (state) => state.cart.items.reduce((total, item) => total + item.price, 0);
-
-// export default cartSlice.reducer;
