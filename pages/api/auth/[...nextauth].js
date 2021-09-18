71b149255426d4a2fb803766a0c0883481009f44
Omar <omaar_5@hotmail.com>
2021-09-18 22:32:35 +0200

    fix: nextAuth errors

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
 export default NextAuth({
     providers: [
-        GoogleProvider({
+        Providers.Google({
             clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        })
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
     ],
 });
