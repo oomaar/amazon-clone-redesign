c197dc011b6b810228ad91c05f0899ad1692a27a
Omar <omaar_5@hotmail.com>
2021-09-18 21:53:22 +0200

    refactor: Sidebar

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -24,47 +24,49 @@ export const Sidebar = () => {
     const [settings, setSettings] = useState(false);
 
     return (
-        <Nav>
-            <Logo onClick={() => router.push('/')}>
-                <Image
-                    src={`${darkValue === 'light' ? "/logo-single-black.png" : "/logo-single-white.png"}`}
-                    width={100}
-                    height={100}
-                    objectFit="contain"
-                />
-            </Logo>
-            <Icons>
-                <li>
-                    <Icon onClick={() => router.push('/')}>
-                        {router.route === '/' ? <AiFillHome /> : <AiOutlineHome />}
-                    </Icon>
-                </li>
-                <li>
-                    <Icon className="relative__icon" onClick={() => router.push('/cart')}>
-                        {router.route === '/cart' ? <HiShoppingCart /> : <HiOutlineShoppingCart />}
-                        <ItemsCount>
-                            <p>{items.length}</p>
-                        </ItemsCount>
-                    </Icon>
-                </li>
-                <li>
-                    <Icon className="relative__icon" onClick={() => router.push("/bookmark")}>
-                        {router.route === '/bookmark' ? <BsBookmarksFill /> : <BsBookmarks />}
-                        <ItemsCount>
-                            <p>0</p>
-                        </ItemsCount>
-                    </Icon>
-                </li>
-                <li>
-                    <Icon>
-                        {router.route === '/checkout' ? <AiFillClockCircle /> : <AiOutlineClockCircle />}
-                    </Icon>
-                </li>
-            </Icons>
-            <Icon onClick={() => setSettings(state => !state)}>
-                {settings ? <RiSettings4Fill /> : <RiSettings5Line />}
-            </Icon>
-            {settings && <Settings />}
-        </Nav>
+        <>
+            <Nav>
+                <Logo onClick={() => router.push('/')}>
+                    <Image
+                        src={`${darkValue === 'light' ? "/logo-single-black.png" : "/logo-single-white.png"}`}
+                        width={100}
+                        height={100}
+                        objectFit="contain"
+                    />
+                </Logo>
+                <Icons>
+                    <li>
+                        <Icon onClick={() => router.push('/')}>
+                            {router.route === '/' ? <AiFillHome /> : <AiOutlineHome />}
+                        </Icon>
+                    </li>
+                    <li>
+                        <Icon className="relative__icon" onClick={() => router.push('/cart')}>
+                            {router.route === '/cart' ? <HiShoppingCart /> : <HiOutlineShoppingCart />}
+                            <ItemsCount>
+                                <p>{items.length}</p>
+                            </ItemsCount>
+                        </Icon>
+                    </li>
+                    <li>
+                        <Icon className="relative__icon" onClick={() => router.push("/bookmark")}>
+                            {router.route === '/bookmark' ? <BsBookmarksFill /> : <BsBookmarks />}
+                            <ItemsCount>
+                                <p>0</p>
+                            </ItemsCount>
+                        </Icon>
+                    </li>
+                    <li>
+                        <Icon>
+                            {router.route === '/checkout' ? <AiFillClockCircle /> : <AiOutlineClockCircle />}
+                        </Icon>
+                    </li>
+                </Icons>
+                <Icon onClick={() => setSettings(true)}>
+                    <RiSettings5Line />
+                </Icon>
+            </Nav>
+            {settings && <Settings setSettings={setSettings} />}
+        </>
     );
 };
