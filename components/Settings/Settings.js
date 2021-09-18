00ab893039dd36b3618eecf8904a1a2b2121594e
Omar <omaar_5@hotmail.com>
2021-09-18 22:00:29 +0200

    style: Settings tab

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -1,11 +1,90 @@
+import { ImCross, ImUser } from "react-icons/im";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { BiSun, BiMoon } from "react-icons/bi";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectDark, setDarkMode } from "../../Redux/slices/darkSlice";
 import {
     Container,
+    Background,
+    SettingsSidebar,
+    GreatingBox,
+    Icon,
+    SubContainer,
 } from "./styledSettings";
 
-export const Settings = () => {
+export const Settings = ({ setSettings }) => {
+    const dispatch = useDispatch();
+    const darkValue = useSelector(selectDark);
+
+    const checkTheme = () => {
+        switch (darkValue) {
+            case "light":
+                dispatch(setDarkMode("dark"));
+                return localStorage.setItem("darkValue", "dark")
+
+            case "dark":
+                dispatch(setDarkMode("light"));
+                return localStorage.setItem("darkValue", "light")
+
+            default:
+                dispatch(setDarkMode("light"));
+                return localStorage.setItem("darkValue", "light")
+        };
+    };
+
+    const handleLight = () => {
+        checkTheme();
+        dispatch(setDarkMode("light"));
+    };
+
+    const handleDark = () => {
+        checkTheme();
+        dispatch(setDarkMode("dark"));
+    };
+
+    useEffect(() => {
+        if (localStorage.getItem("darkValue")) {
+            const darkerValue = localStorage.getItem("darkValue");
+            dispatch(setDarkMode(darkerValue));
+        } else {
+            checkTheme(null);
+        };
+    });
+
     return (
         <Container>
-            Settings Tab
+            <SettingsSidebar>
+                <GreatingBox>
+                    <span><ImUser /> Hello, Username</span>
+                    <Icon>
+                        <ImCross onClick={() => setSettings(false)} />
+                    </Icon>
+                </GreatingBox>
+                <SubContainer>
+                    <ImUser />
+                    Your Account
+                </SubContainer>
+                <SubContainer>
+                    <RiCustomerService2Fill />
+                    Customer Service
+                </SubContainer>
+                {darkValue === 'light' ? (
+                    <SubContainer onClick={handleDark}>
+                        <BiMoon />
+                        Dark Mode
+                    </SubContainer>
+                ) : (
+                    <SubContainer onClick={handleLight}>
+                        <BiSun />
+                        Dark Mode
+                    </SubContainer>
+                )}
+                <SubContainer>
+                    Sign In
+                </SubContainer>
+            </SettingsSidebar>
+            <Background onClick={() => setSettings(false)}></Background>
         </Container>
     );
 };
--- components/Settings/styledSettings.js
+++ components/Settings/styledSettings.js
@@ -1,3 +1,81 @@
 import styled from 'styled-components';
+import { lgScreen, xlScreen } from '../../Global/GlobalStyle';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 200;
+  display: flex;
+`;
+
+export const Background = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  height: 100%;
+  width: 100%;
+  display: none;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    display: block;
+    width: 50%;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    width: 75%;
+  }
+`;
+
+export const SettingsSidebar = styled.div`
+  background: ${({ theme }) => theme.colors.bodyColor};
+  width: 100%;
+  color: ${({ theme }) => theme.colors.textColor};
+
+  @media screen and (min-width: ${lgScreen}px) {
+    width: 50%;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    width: 25%;
+  }
+`;
+
+export const GreatingBox = styled.div`
+  background-color: ${({ theme }) => theme.colors.containerColorSecond};
+  color: ${({ theme }) => theme.colors.buttonText};
+  padding: 1rem 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: clamp(1.5rem, 2vw, 1.7rem);
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    cursor: pointer;
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const Icon = styled.div`
+  font-size: clamp(1.2rem, 1.6vw, 1.5rem);
+  cursor: pointer;
+`;
+
+export const SubContainer = styled.div`
+  padding: 1rem 0.5rem;
+  font-size: clamp(1.2rem, 1.6vw, 1.5rem);
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  cursor: pointer;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
