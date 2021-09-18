36699f8bc1564ef4f07f528c32967fa2a9ba9299
Omar <omaar_5@hotmail.com>
2021-09-18 22:45:55 +0200

    add: AuthProvider around application

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,18 @@ import { Provider } from "react-redux";
 import { persistor, store } from "../Redux/store";
 import Layout from "../Global/Layout";
 import { PersistGate } from 'redux-persist/integration/react';
+import { Provider as AuthProvider } from "next-auth/client";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </PersistGate>
-    </Provider>
+    <AuthProvider session={pageProps.session}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PersistGate>
+      </Provider>
+    </AuthProvider>
   );
 };
